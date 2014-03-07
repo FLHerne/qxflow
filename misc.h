4cfa9c01d6fc11102674623e517fd6adcaba2a79
Francis Herne <francis.herne@yahoo.co.uk>
2014-03-07 14:30:22 +0000

    Inline misc functions

--- misc.h
+++ misc.h
@@ -3,8 +3,15 @@
 #include <QPointF>
 
 //Round 'in_val' up or down to nearest multiple of 'step'.
-int roundTo(float in_val, int step);
+inline int roundTo(float in_val, int step) {
+    return std::roundf(in_val / step) * step;
+}
+
 //Round x and y values of 'in_point' to nearest multiple of 'step'.
-QPointF roundTo(const QPointF&, int step);
+inline QPointF roundTo(const QPointF& in_point, int step) {
+    if (!step) return in_point;
+    return QPointF(roundTo(in_point.x(), step),
+                   roundTo(in_point.y(), step));
+}
 
 #endif //MISC_H
