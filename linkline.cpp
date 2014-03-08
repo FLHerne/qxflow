995eb2666b2f73960a304817740de9981d7aea9f
Francis Herne <francis.herne@yahoo.co.uk>
2014-03-08 14:38:57 +0000

    New 'linkline' class, will replace current hack

---
+++ linkline.cpp
@@ -0,0 +1,49 @@
+#include "linkline.h"
+#include "misc.h"
+
+//Public constructor
+LinkLineItem::LinkLineItem(const QLineF& line, QGraphicsItem* parent, QGraphicsScene* scene): QGraphicsLineItem(parent, scene) {
+    setLine(line);
+    setPen(QPen(Qt::black, 3));
+}
+
+//Public (hides non-virtual base)
+void LinkLineItem::setLine(const QLineF& line) {
+    QLineF aligned(roundTo(line.p1(), grid_size), roundTo(line.p2(), grid_size));
+    if (aligned.y1() == aligned.y2()) horizontal = true;
+    else {
+        horizontal = false;
+        if (aligned.x1() != aligned.x2()) qDebug() << "Not square!";
+    }
+    QGraphicsLineItem::setLine(aligned);
+}
+
+//Public virtual
+void LinkLineItem::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) {
+    QGraphicsLineItem::paint(painter, option, widget);
+    auto i = nodes.begin();
+    while (i != nodes.end()) {
+        if (!(*i)->highlighted()) {
+            delete *i;
+            i = nodes.erase(i);
+        } else ++i;
+    }
+    QGraphicsItem* cur_item;
+    LinkNodeItem* cur_node;
+    QPointF aligned, new_node_pos;
+    bool node_exists;
+    foreach (cur_item, collidingItems(Qt::IntersectsItemBoundingRect)) {
+        if (cur_node = qgraphicsitem_cast<LinkNodeItem*>(cur_item)) {
+            aligned = roundTo(cur_node->sceneCenterPos(), grid_size);
+            new_node_pos = QPointF(horizontal ? aligned.x() : line().p1().x(), horizontal ? line().p1().y() : aligned.y());
+            node_exists = false;
+            foreach (cur_node, nodes) {
+                if (cur_node->centerPos() == new_node_pos) {
+                    node_exists = true;
+                    break;
+                }
+            }
+            if (!node_exists) nodes.append(new LinkNodeItem(new_node_pos.x(), new_node_pos.y(), this));
+        }
+    }
+}
---
+++ linkline.h
@@ -0,0 +1,22 @@
+#ifndef LINKLINE_H
+#define LINKLINE_H
+
+#include "linknode.h"
+
+class LinkLineItem : public QGraphicsLineItem {
+public:
+    LinkLineItem(const QLineF& line, QGraphicsItem* parent = 0, QGraphicsScene* scene = 0);
+//Sets line rounded to grid.
+    void setLine(const QLineF& line);
+//Creates child LinkNodes where other LinkNodes overlap.
+    virtual void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget = 0);
+private:
+//List of child linknodes, to avoid re-adding existing ones.
+    QLinkedList<LinkNodeItem*> nodes;
+//Spacing of grid, if parent is a ChartScene.
+    int grid_size = 20;
+//Whether line is horizontal (otherwise should be vertical).
+    bool horizontal;
+};
+
+#endif //LINKLINE_H
